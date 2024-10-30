16003a976f24f1b9b555742350cd1f8c85563694
chloé Nieuwjaer <Cnieuwjaer@gmail.com>
2024-10-30 16:24:25 +0000

    XXX_VDEF

--- models/intermediate/merged_ads_regie.sql
+++ models/intermediate/merged_ads_regie.sql
@@ -1,19 +1,26 @@
 --- On merge tous les datasets publicitaires au sein d'un document unique
 
-SELECT *
-FROM {{ ref('stg_raw__adword') }}
+-- SELECT *
+-- FROM {{ ref('stg_raw__adword') }}
 
-UNION ALL
+-- UNION ALL
 
-SELECT *
-FROM {{ ref('stg_raw__bing') }}
+-- SELECT *
+-- FROM {{ ref('stg_raw__bing') }}
 
-UNION ALL
+-- UNION ALL
 
-SELECT *
-FROM {{ ref('stg_raw__criteo') }}
+-- SELECT *
+-- FROM {{ ref('stg_raw__criteo') }}
 
-UNION ALL
+-- UNION ALL
 
-SELECT *
-FROM {{ ref('stg_raw__facebook') }}
+-- SELECT *
+-- FROM {{ ref('stg_raw__facebook') }}
+
+{{ dbt_utils.union_relations(
+    relations=[ref('stg_raw__adword'),
+    ref('stg_raw__facebook'),
+    ref('stg_raw__bing'),
+    ref('stg_raw__criteo')]
+) }}
